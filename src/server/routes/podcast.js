11343b071dbead27f5f8c3272df4ac971131a1d7
Dewitt Buckingham <dj729@comcast.net>
2018-03-27 19:06:39 -0700

    Add Cover Art and Slug

--- src/server/routes/podcast.js
+++ src/server/routes/podcast.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+const multer = require('multer');
 const { Podcast } = require('../controllers');
+
+const upload = multer();
 
 const router = Router();
 
-router.post('/podcast', Podcast.CREATE_PODCAST);
+router.post('/podcast', upload.single('cover'), Podcast.CREATE_PODCAST);
 
 module.exports = router;
