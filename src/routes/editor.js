500f2cb4df1b2e332a3e27ebc77d53ee52c1edb1
Dewitt Buckingham <dj729@comcast.net>
2018-04-10 14:14:05 -0700

    Editor/Preview
    Close #10
    
    Will have to wait for the Markdown functionality.

---
+++ src/routes/editor.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/editor', renderEditor);
+router.post('/editor/preview', preview);
+
+module.exports = router;
+
+function renderEditor(req, res) {
+  res.render('editor');
+}
+
+function preview(req, res) {
+  res.render('preview', { preview: req.body, user: req.user });
+}
