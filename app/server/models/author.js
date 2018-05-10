f3694eef062a6f70e9d655ba803063ce2e394c7c
Dewitt Buckingham <dj729@comcast.net>
2018-05-09 23:50:50 -0700

    Update Models

--- app/server/models/author.js
+++ app/server/models/author.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
 const token = require('random-token');
+const shortid = require('shortid');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const authorSchema = new Schema({
+  uid: {
+    type: String,
+    required: true,
+    default: shortid.generate,
+    unique: true
+  },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   displayName: { type: String, default: this.username },
   token: { type: String, default: token(128) },
-  created: { type: Date, default: Date.now() }
+  createdDate: { type: Date, default: Date.now() }
 });
 
-userSchema.plugin(uniqueValidator);
+authorSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Author', authorSchema);
