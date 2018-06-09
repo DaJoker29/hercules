84f58263769f6da9001da842e4b2469ac0b2479c
Dewitt Buckingham <dj729@comcast.net>
2018-06-08 18:24:26 -0700

    Update Author

--- app/server/routes/authors.js
+++ app/server/routes/authors.js
@@ -6,8 +6,35 @@ const router = new Router();
 router.get('/authors', fetchAllAuthors);
 router.get('/author/:id', fetchSingleAuthor);
 router.post('/authors', createNewAuthor); // Add Authentication
+router.put('/author/:id', updateAuthor); // Add Authentication
 
 module.exports = router;
+
+async function updateAuthor(req, res, next) {
+  const { id } = req.params;
+  const update = {};
+  if (req.body.email) {
+    update.email = req.body.email;
+  }
+
+  if (req.body.displayName) {
+    update.displayName = req.body.displayName;
+  }
+
+  try {
+    const updated = await Author.findOneAndUpdate({ uid: id }, update, {
+      new: true
+    });
+    const author = await Author.findOne({ uid: updated.uid });
+    const result = Object.assign(
+      { postURL: `/api/posts?author=${author.uid}` },
+      JSON.parse(JSON.stringify(author))
+    );
+    res.json(result);
+  } catch (e) {
+    next(e);
+  }
+}
 
 async function createNewAuthor(req, res, next) {
   const { email, username, displayName } = req.body;
