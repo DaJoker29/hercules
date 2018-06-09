92f9471caf0796d842654127d3285f403fbb4c09
Dewitt Buckingham <dj729@comcast.net>
2018-06-08 17:50:38 -0700

    Fetch Single Author

--- app/server/routes/authors.js
+++ app/server/routes/authors.js
@@ -1,26 +1,21 @@
 const { Router } = require('express');
-const passport = require('passport');
-const { Author, Post } = require('../models');
+const { Author } = require('../models');
 
 const router = new Router();
 
 router.get('/authors', fetchAllAuthors);
-router.get(
-  '/user/:username',
-  passport.authenticate('jwt', { session: false }),
-  fetchSingleAuthor
-);
+router.get('/author/:id', fetchSingleAuthor);
 
 module.exports = router;
 
 async function fetchSingleAuthor(req, res, next) {
-  const { username } = req.params;
+  const { id } = req.params;
   try {
-    const user = await Author.find({ username });
-    const posts = await Post.find({ 'author._id': user._id }).populate(
-      'author'
+    const author = await Author.findOne({ uid: id });
+    const result = Object.assign(
+      { postURL: `/api/posts?author=${author.uid}` },
+      JSON.parse(JSON.stringify(author))
     );
-    const result = Object.assign({ posts, user });
     res.json(result);
   } catch (e) {
     next(e);
